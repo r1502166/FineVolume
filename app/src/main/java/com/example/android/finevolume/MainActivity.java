efefb5a8bc4fa9cb47ba08475f6fecf551c1e157
Canal Kim <canalkim@git.com>
2018-10-08 13:29:55 -0700

    Done v1.0

--- app/src/main/java/com/example/android/finevolume/MainActivity.java
+++ app/src/main/java/com/example/android/finevolume/MainActivity.java
@@ -1,13 +1,57 @@
 package com.example.android.finevolume;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AudioManager audio;
+    private int currentVolume;
+    private int maxVolume;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        audio = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        maxVolume = audio.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        currentVolume = audio.getStreamVolume(AudioManager.STREAM_MUSIC);
+        displayVolume();
+
+        Button btnMinus = findViewById(R.id.minus_button);
+        btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if (currentVolume > 0) {
+                    currentVolume--;
+                }
+                displayVolume();
+                audio.setStreamVolume(AudioManager.STREAM_MUSIC, currentVolume, 0);
+            }
+        });
+
+        Button btnPlus = findViewById(R.id.plus_button);
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if (currentVolume < maxVolume) {
+                    currentVolume++;
+                }
+                displayVolume();
+                audio.setStreamVolume(AudioManager.STREAM_MUSIC, currentVolume, 0);
+            }
+        });
+
+    }
+
+    private void displayVolume() {
+        TextView label = findViewById(R.id.volume_textView);
+        label.setText(Integer.toString(currentVolume)+'/'+Integer.toString(maxVolume));
     }
 }
